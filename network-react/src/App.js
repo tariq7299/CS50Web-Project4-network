916918c90701379e35a02d0221ccb1a586475db5
tariq7299 <tr.sar77an78@gmail.com>
2023-12-30 05:31:59 +0200

    Adjusted some css for side nav bar and for the Post modal

--- network-react/src/App.js
+++ network-react/src/App.js
@@ -22,15 +22,24 @@ function App() {
 
       <div className="navbar">
         <div className="side-navbar">
-         <a href="#">Network Project</a>
-         <a href="#">Home</a>
-         <a href="#">Logout</a>
-         <button className="new-post-button" onClick={handelPostModal}>Post</button>
-         <div className="user-info">
-          <img className="profile-image" src="default-profile.svg.png" alt="user-profile-image"></img>
-          <p className="user-actual-name">Tariq Sarhan</p>
-          <p className="username">teka</p>
+
+          <div className="nav-buttons-and-post-button-wrapper">
+            <a href="#">Network Project</a>
+            <a href="#">Home</a>
+            <a href="#">Logout</a>
+            <button className="new-post-button" onClick={handelPostModal}>Post</button>
           </div>
+
+          <div className="user-info-wrapper">
+            <div className="profile-image-wrapper">
+              <img className="profile-image" src="default-profile.svg.png" alt="user-profile-image"></img>
+            </div>
+            <div className="username-wrapper">              
+            <p className="user-actual-name">Tariq Sarhan</p>
+            <p className="username">teka</p>
+            </div>
+          </div>
+          
         </div>
         <div className= "top-navbar">
           <a href="#"> for you</a>
