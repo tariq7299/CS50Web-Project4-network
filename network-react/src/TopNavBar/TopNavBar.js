b4f9b179d5202ba7844725fde2c2520163ae2f07
tariq7299 <tr.sar77an78@gmail.com>
2024-02-04 13:31:46 +0200

    styled each post

--- network-react/src/TopNavBar/TopNavBar.js
+++ network-react/src/TopNavBar/TopNavBar.js
@@ -4,28 +4,14 @@ import { useAuth } from "../hooks/AuthProvider";
 
 export default function TopNavBar({handelPostModal}) {
     
-    const navigate = useNavigate();
-    const auth = useAuth();
-
-    function handleLogout() {
-        auth.logOut()
-
-        return
-        
-    }
-    
     const userData = JSON.parse(localStorage.getItem("userData"))
 
     return (
         <div className="top-navbar-wrapper">
-
             <div className= "top-navbar">
-                <Link to="/dashboard"> for you</Link>
+                <Link to="/dashboard"> For you</Link>
                 <Link to="/following"> Following</Link>
             </div>
-
-            
-          
         </div>
     )
 
