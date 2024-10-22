db5a8642c5bdc3294a801fdaf25e98de7dca740b
tariq7299 <tr.sar77an78@gmail.com>
2024-10-22 11:16:36 +0300

    This is the version of the project which I have submitted to CS50Web to get the Cert

--- project4/network/static/network/src/App.js
+++ project4/network/static/network/src/App.js
@@ -1,8 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./routes/Login/Login";
-import Register from "./routes/Register/Register";
-import Feed from "./routes/Feed/Feed";
-import Profile from "./routes/Profile/Profile";
 import AuthProvider from "./hooks/AuthProvider";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
 // import Root from "./routes/Root/Root";
@@ -16,22 +12,9 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <CurrentViewProvider>
-
           <Routes>
-            {/* <Route path="/register" element={<Register />} /> */}
-            {/* <Route path="/login" element={<Login />} /> */}
             <Route element={<PrivateRoute />}>
-              {/* <Root /> */}
               <Route path="/" element={<Root />} />
-
-              {/* <Route path="/" element={<Root />}>
-                <Route index element={<Feed />} />
-                <Route path="/dashboard" element={<Feed />} />
-                <Route path="/following" element={<Feed />} />
-                <Route path="/profile/:username" element={<Profile />} />
-                
-                </Route>
-                {/* Other routes */}
             </Route>
           </Routes>
         </CurrentViewProvider>
